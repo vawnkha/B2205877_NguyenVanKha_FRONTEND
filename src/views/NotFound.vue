1303d77862d8ad37035237b9c9b0044f62f03ffb
vawnkha <nguyen4725kha@gmail.com>
2025-10-28 17:01:45 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oops, Không thể tìm thấy trang. Trở về
+            <router-link to="/">Trang chủ.</router-link>
+        </p>
+    </div>
+</template>
